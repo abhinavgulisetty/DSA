79c9e6d4fba9c1ec11155e7f7f23aec5e502a4e5
Gulisetty Abhinav <abhinavgulisetty@gmail.com>
2025-06-05 21:00:04 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (83.76%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<int, unordered_set<char>> rows, cols;
-        map<pair<int, int>, unordered_set<char>> squares;
+        int rows[9] = {0};
+        int cols[9] = {0};
+        int squares[9] = {0};
 
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
                 if (board[r][c] == '.') continue;
-                
-                pair<int, int> squareKey = {r / 3, c / 3};
 
-                if (rows[r].count(board[r][c]) || cols[c].count(board[r][c]) || squares[squareKey].count(board[r][c])) {
+                int val = board[r][c] - '1';
+
+                if ((rows[r] & (1 << val)) || (cols[c] & (1 << val)) ||
+                    (squares[(r / 3) * 3 + (c / 3)] & (1 << val))) {
                     return false;
                 }
 
-                rows[r].insert(board[r][c]);
-                cols[c].insert(board[r][c]);
-                squares[squareKey].insert(board[r][c]);
+                rows[r] |= (1 << val);
+                cols[c] |= (1 << val);
+                squares[(r / 3) * 3 + (c / 3)] |= (1 << val);
             }
         }
         return true;
