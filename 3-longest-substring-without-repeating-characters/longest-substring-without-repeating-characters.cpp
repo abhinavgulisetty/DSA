606fd72430f8fb0324cf25d47f61b9140b1a7408
Gulisetty Abhinav <abhinavgulisetty@gmail.com>
2025-06-13 22:13:17 +0530

    Time: 19 ms (30.59%) | Memory: 14.3 MB (22.49%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,13 +4,13 @@ public:
         unordered_set<char> a;
         int ans = 0;
         int l = 0;
-        for(int r = 0; r < s.size();r++){
+        for(int r=0;r<s.size();r++){
             while(a.find(s[r]) != a.end()){
                 a.erase(s[l]);
                 l++;
             }
             a.insert(s[r]);
-            ans = max(ans, r - l + 1);
+            ans = max(ans,r-l+1);
         }
         return ans;
     }
